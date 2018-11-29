3af9538af2c9ee7ff6beb22f4d6cb025a64fb86f
Songyuqin <123@qq.com>
2018-11-29 20:29:46 +0800

    查不到

--- routes/phone.js
+++ routes/phone.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var multer=require("multer");
+var upload=multer({dest:"c:/tmp"})
 var router = express.Router();
+var fs=require('fs');
+var path=require
 var MongoClient=require('mongodb').MongoClient;
 var ObjectId=require('mongodb').ObjectId;
 var async=require('async');
+
 var url='mongodb://localhost:27017';
 // 数据的列表数据;
 router.get("/",function(req,res){
@@ -93,9 +98,53 @@ router.get('/delete',function(req,res){
   })
   
   })
+// 新增手机
+router.post('/addPhone',upload.single('src'),function(req,res){
+console.log(req.file);
+console.log("");
+res.send("");
+
+
+})
 
 
 
+
+
+// 修改手机列表数据；
+router.get('/updata',function(req,res){
+  
+  var id=req.query.id;
+  
+  console.log(id);
+  res.send("");
+  // MongoClient.connect(url,{useNewUrlParser:true},function(err,client){
+  //   if(err){
+  //     res.render('error',{
+  //       message:'链接失败',
+  //       error:err
+  //     })
+  //     return;
+  //   }
+  //   var db=client.db('project');
+  //   db.collection('phone').deleteOne({
+  //     _id:ObjectId(id),
+  
+  
+  //   },function(err){
+  //     if(err){
+  //       res.render('error',{
+  //         message:'删除失败',
+  //         error:err
+  //       })
+  //     }else{
+  //       res.redirect('/phone');
+  //     }
+  //     client.close();
+  //   })
+  // })
+  
+  })
 
 
 
